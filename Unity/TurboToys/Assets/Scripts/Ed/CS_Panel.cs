4a5130dcc92a8a9604f78954e798d25e8a58d69b
FunBinge <cardenas.eduardo10@yahoo.com.au>
2015-11-19 14:37:10 +1030

    UI_PressAToJoin

---
+++ Unity/TurboToys/Assets/Scripts/Ed/CS_Panel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_Panel : MonoBehaviour {
+
+    public GameObject activeScreen;
+    public GameObject inactiveScreen;
+    public bool activated = false;
+
+    public void Activate()
+    {
+        activeScreen.SetActive(true);
+        inactiveScreen.SetActive(false);
+        activated = true;
+    }
+}
---
+++ Unity/TurboToys/Assets/Scripts/Ed/UI_InputManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using InControl;
+public class UI_InputManager : MonoBehaviour {
+
+    public List<GameObject> playerCursors = new List<GameObject>();
+    public List<CS_Panel> playerPanels = new List<CS_Panel>();
+
+    private List<InputDevice> inputDevice = new List<InputDevice>();
+
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < InputManager.Devices.Count; i++ )
+        {
+            inputDevice.Add(InputManager.Devices[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 RS = inputDevice[0].RightStick;
+        Debug.Log(InputManager.Devices.Count);
+
+        for (int i = 0; i < InputManager.Devices.Count; i++)
+        {
+            if (InputManager.Devices[i] && InputManager.Devices[i].Action1 && playerPanels[i] && !playerPanels[i].activated)
+            {
+                playerPanels[i].Activate();
+            }
+        }
+	}
+}
