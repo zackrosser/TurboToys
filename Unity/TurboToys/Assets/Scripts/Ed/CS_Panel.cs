c635e7515fd2a62a6f12d74d865c6f9000bf0751
Ed <cardenas.eduardo10@yahoo.com.au>
2015-11-19 17:24:25 +1030

    push

--- Unity/TurboToys/Assets/Scripts/Ed/CS_Panel.cs
+++ Unity/TurboToys/Assets/Scripts/Ed/CS_Panel.cs
@@ -5,12 +5,15 @@ public class CS_Panel : MonoBehaviour {
 
     public GameObject activeScreen;
     public GameObject inactiveScreen;
+    public CharacterSelectCursor cursor;
     public bool activated = false;
 
     public void Activate()
     {
+        Debug.Log("activated" + name);
         activeScreen.SetActive(true);
         inactiveScreen.SetActive(false);
+        cursor.gameObject.SetActive(true);
         activated = true;
     }
 }
