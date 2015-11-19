6510f08014586ed96408045b82164d46d0b5e569
Natalie <warcraftcats@hormail.co.uk>
2015-11-19 15:59:48 +1030

    Main Menu
    Particles play when you click Play/Quit.

---
+++ Unity/TurboToys/Assets/UI/Scripts/ParticleStopper.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleStopper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<ParticleSystem>().Stop();
+	}
+	    
+}
